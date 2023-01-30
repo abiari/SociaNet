48b4245475d396f6ddc74e9d54420e36898556f9
Ali Biari <abiari@leyton.com>
2023-01-30 23:26:18 +0100

    Add registration

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { register } from './controllers/auth.js';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -14,7 +15,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json({ limit: '30mb' }));
-app.use(express.urlencoded({ limit: '30mb' }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
@@ -24,7 +25,7 @@ app.use('/assets', express.static(path.join(__dirname, '/assets')));
 /* FILE STORAGE */
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, '/assets');
+    cb(null, './src/assets');
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
@@ -33,5 +34,14 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+app.post('/auth/register', upload.single('picture'), register);
+
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT
+const PORT = process.env.PORT || '6001';
+const MONGO_URL = process.env.MONGO_URL as string;
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
