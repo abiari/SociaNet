411b68394187e552cea775b460045bdb05c6a2b8
Ali Biari <abiari@leyton.com>
2023-02-05 16:21:29 +0100

    update User schema def

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,58 +1,60 @@
-import mongoose, { Document } from 'mongoose';
+import mongoose, { Document, Schema } from 'mongoose';
 
-// interface IUser extends Document {
-//   firstName: string;
-//   lastName: string;
-//   email: string;
-//   password: string;
-//   picturePath: string;
-//   friends: any[];
-//   location: string;
-//   occupation: string;
-//   viewedProfile: number;
-//   impressions: number;
-// }
+export interface IUser {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  picturePath: string;
+  friends: any[];
+  location: string;
+  occupation: string;
+  viewedProfile: number;
+  impressions: number;
+}
 
-const UserSchema = new mongoose.Schema (
+export interface IUserModel extends IUser, Document {}
+
+const UserSchema: Schema = new Schema(
   {
     firstName: {
       type: String,
       required: true,
       min: 2,
-      max: 50,
+      max: 50
     },
     lastName: {
       type: String,
       required: true,
       min: 2,
-      max: 50,
+      max: 50
     },
     email: {
       type: String,
       required: true,
       max: 50,
-      unique: true,
+      unique: true
     },
     password: {
       type: String,
       required: true,
-      min: 6,
+      min: 6
     },
     picturePath: {
       type: String,
-      default: '',
+      default: ''
     },
     friends: {
-      type: Array,
-      default: [],
+      type: [],
+      default: []
     },
     location: String,
     occupation: String,
     viewedProfile: Number,
-    impressions: Number,
+    impressions: Number
   },
-  { timestamps: true },
+  { timestamps: true }
 );
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model<IUserModel>('User', UserSchema);
 export default User;
